148de937afb7759a0dbf0bf409ad1e4e1f761e94
Robpol86 <robpol86@gmail.com>
2021-06-13 10:54:15 -0700

    fix logging

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ IMPORT = 'sphinxcontrib.imgur'
 INSTALL_REQUIRES = ['requests', 'sphinx']
 LICENSE = 'MIT'
 NAME = 'sphinxcontrib-imgur'
-VERSION = '2.0.2.dev0'
+VERSION = '2.0.2.dev1'
 
 
 def readme(path='README.rst'):
